3508e8b66bdce8e72c9c70983a36d46b148e3254
Cormac Pujals <cmpujals@gmail.com>
2022-10-28 16:04:24 -0700

    step 9 complete

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,38 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+// Google OAuth login route
+router.get('/auth/google', passport.authenticate(
+  'google',
+  {
+    scope: ['profile', 'email'],
+    // optional
+    // prompt: 'select_account'
+  }
+));
+
+// Google OAuth callback route
+router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+    successRedirect: '/movies',
+    failureRedirect: '/movies'
+  }
+));
+
+// OAuth logout route
+router.get('/logout', function(req, res){
+  req.logout(function() {
+    res.redirect('/movies');
+  });
+});
+
 
 module.exports = router;
